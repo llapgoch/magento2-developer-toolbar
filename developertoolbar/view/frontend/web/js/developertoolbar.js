83056ca2ad80dd05d3e234b832053c8830aef5ec
Dave Baker <dbaker@iweb.co.uk>
2016-02-20 16:40:57 +0000

    Block output

--- developertoolbar/view/frontend/web/js/developertoolbar.js
+++ developertoolbar/view/frontend/web/js/developertoolbar.js
@@ -4,6 +4,18 @@
     }
     
     $(document).ready(function(){
-        
+        $('.js-developertoolbar__button-link').on('click', function(ev){
+            ev.preventDefault();
+
+            var $item = $(this).closest('.js-developertoolbar__item'),
+                active = $item.hasClass('developertoolbar__item--active'); 
+         
+            $('.developertoolbar__item').removeClass('developertoolbar__item--active');
+  
+            if(!active){  
+                $item.addClass('developertoolbar__item--active');
+            }
+
+        });
     });
 }(jQuery));
