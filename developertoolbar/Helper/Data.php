037ee6953a7c7d8849bf2e52cdf64f767ebe3af8
Dave Baker <dbaker@iweb.co.uk>
2016-02-26 08:32:46 +0000

    Block viewer updates

--- developertoolbar/Helper/Data.php
+++ developertoolbar/Helper/Data.php
@@ -2,7 +2,71 @@
 namespace Llapgoch\Developertoolbar\Helper;
 
 class Data extends \Magento\Framework\App\Helper\AbstractHelper{
+    const START_MARKER_SUFFIX = "-start-viewer";
+    const END_MARKER_SUFFIX = "-end-viewer";
+    const GLOBAL_MARKER = "developer-toolbar-dom-marker";
+    const HEAD_COMPONENTS_NAME = "head.components";
+    const DEVELOPER_TOOLBAR_NAME = "llapgoch.developertoolbar";
+    
+    protected $_layout;
+    protected $_dataStructure;
+    
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Framework\View\Layout $layout){
+            parent::__construct($context);
+            $this->_layout = $layout;
+            $this->_dataStructure = $layout->getStructure();
+    }
+    
     public function makeLayoutNameIntoClass($name){
         return str_replace('.', '-', $name);
+    }
+    
+    public function makeStartMarker($blockName){
+        return $this->_helper->makeLayoutNameIntoClass($blockName) . self::START_MARKER_SUFFIX . " " . self::GLOBAL_MARKER;
+    }
+    
+    public function makeEndMarker($blockName){
+        return $this->_helper->makeLayoutNameIntoClass($blockName) . self::END_MARKER_SUFFIX . " " . self::GLOBAL_MARKER;
+    }
+    
+    public function getForbiddenNames(){
+        return array(self::DEVELOPER_TOOLBAR_NAME, self::HEAD_COMPONENTS_NAME);
+    }
+    
+    public function wrapContent($layoutName, $content){
+        if($this->_isForbidden($layoutName, $this->getForbiddenNames())){
+            return $content;
+        }
+        
+        return "<!--" . $this->_helper->makeStartMarker($layoutName) . "-->" . $content . "<!--" . $this->_helper->makeEndMarker($layoutName) . "-->";
+    }
+    
+    protected function _isForbidden($layoutName, $forbidden = array()){     
+        if(in_array($layoutName, $forbidden)){
+            return true;
+        }
+        
+        // Don't wrap for AJAX calls
+        if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) 
+            && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+            return true;
+        }
+        
+        // Don't wrap for forbidden blocks
+        return $this->_isParentOf($layoutName, $forbidden);
+    }
+    
+    protected function _isParentOf($layoutName, $potentialParents = array()){
+        if(!$layoutName){
+            return false;
+        }
+        
+        if(in_array($layoutName, $potentialParents)){
+            return true;
+        }
+        
+        return $this->_isParentOf($this->_dataStructure->getParentId($layoutName), $potentialParents);
     }
 }
--- developertoolbar/Model/View/Layout.php
+++ developertoolbar/Model/View/Layout.php
@@ -1,8 +1,85 @@
 <?php
 namespace Llapgoch\Developertoolbar\Model\View;
 
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Framework\Message\ManagerInterface as MessageManagerInterface;
+use Magento\Framework\Cache\FrontendInterface;
+use Magento\Framework\App\State as AppState;
+use Psr\Log\LoggerInterface as Logger;
+use Magento\Framework\View\Layout\
+    Element;
+
 class Layout extends \Magento\Framework\View\Layout{
+    protected $_helper;
+    
+    public function __construct(
+        \Magento\Framework\View\Layout\ProcessorFactory $processorFactory,
+        ManagerInterface $eventManager,
+        \Magento\Framework\View\Layout\Data\Structure $structure,
+        MessageManagerInterface $messageManager,
+        \Magento\Framework\View\Design\Theme\ResolverInterface $themeResolver,
+        \Magento\Framework\View\Layout\ReaderPool $readerPool,
+        \Magento\Framework\View\Layout\GeneratorPool $generatorPool,
+        FrontendInterface $cache,
+        \Magento\Framework\View\Layout\Reader\ContextFactory $readerContextFactory,
+        \Magento\Framework\View\Layout\Generator\ContextFactory $generatorContextFactory,
+        AppState $appState,
+        Logger $logger,
+        $cacheable = true,
+        \Llapgoch\Developertoolbar\Helper\Data $helper){
+        
+        parent::__construct(
+            $processorFactory,
+            $eventManager,
+            $structure,
+            $messageManager,
+            $themeResolver,
+            $readerPool,
+            $generatorPool,
+            $cache,
+            $readerContextFactory,
+            $generatorContextFactory,
+            $appState,
+            $logger,
+            $cacheable
+        );
+        
+         $this->_helper = $helper;
+    }
+    
+    // If only this was public to begin with!
     public function getStructure(){
         return $this->structure;
+    }
+    
+    protected function _renderContainer($name)
+    {
+        $html = '';
+        $children = $this->getChildNames($name);
+        foreach ($children as $child) {
+            $html .= $this->renderElement($child);
+        }
+        if ($html == '' || !$this->structure->getAttribute($name, Element::CONTAINER_OPT_HTML_TAG)) {
+            return $html;
+        }
+
+        $htmlId = $this->structure->getAttribute($name, Element::CONTAINER_OPT_HTML_ID);
+        if ($htmlId) {
+            $htmlId = ' id="' . $htmlId . '"';
+        }
+        
+        $htmlClass = $this->structure->getAttribute($name, Element::CONTAINER_OPT_HTML_CLASS);
+        if ($htmlClass) {
+            $htmlClass = ' class="' . $htmlClass . '"';
+        }
+
+        $htmlTag = $this->structure->getAttribute($name, Element::CONTAINER_OPT_HTML_TAG);
+        
+        // This is the change we need to make for the developer toolbar - if only we could use a plugin for this!!
+        
+
+        $html = sprintf('<%1$s%2$s%3$s>%4$s</%1$s>', $htmlTag, $htmlId, $htmlClass, $html);
+
+        return $html;
     }
 }
